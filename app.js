55480bb060706ba0cfd6438f99116a5de5478043
0u0 <jacky8399@users.noreply.github.com>
2018-05-26 16:54:33 +0800

    Unified whitelist operations

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const iconv = require('iconv-lite')
 
 encode = (str) => (new Buffer(iconv.encode(str, 'gbk'))).toString('base64')
 
+async function checkIsInWhitelist(uname) {
+	var uList = await doufu.getWhiteList();
+	var list = JSON.parse(uList["players"]);
+	return list.indexOf(uname) > -1;
+}
+
 app.use(async (ctx, next) => {
     const { content } = ctx
     if (content[0] === 'GroupMessage' && (content[2] === '583037517' || content[2] === '730455691')) {
@@ -20,7 +26,26 @@ app.use(async (ctx, next) => {
             try {
                 const message = iconv.decode(new Buffer(content[5], 'base64'), 'gbk').replace(/&#91;/g, '[').replace(/&#93;/g, ']').replace(/&amp;/g, '&').split(" ")
                 console.log(message)
-                if(message[0][0] !== '#白名单' && message.length === 3) {
+				if (message[0] === "#白名单" && message.length === 2) {
+					console.log("whitelist handler");
+					var user = message[1];
+					if (/^[a-zA-Z_][a-zA-Z0-9_]{2,11}$/.test(user)) {
+						checkIsInWhitelist(user).then(async function (bool){ 
+							if (bool) {
+								// Remove from whitelist
+								await doufu.deleteWhiteName(user)
+								ctx.app.send(`GroupMessage ${content[2]} ${encode(`玩家${user}在白名单中,并已成功删除.\n回滚操作请重复'${message.join(' ')}'`)}`)
+							} else {
+								// Add to whitelist
+								await doufu.addWhiteName(user)
+								ctx.app.send(`GroupMessage ${content[2]} ${encode(`玩家${user}不在白名单中,并已成功添加.\n回滚操作请重复'${message.join(' ')}'`)}`)
+							}
+						});
+					} else {
+                        ctx.app.send(`GroupMessage ${content[2]} ${encode(`${user}名称不合法, 只能由0-9,a-z以及下划线组成, 不能超过12字符`)}`)
+                    }
+				}
+                /*if(message[0][0] !== '#白名单' && message.length === 3) {
                     if(message[1] === '添加') {
                         if (/^[a-zA-Z_][a-zA-Z0-9_]{2,11}$/.test(message[2])) {
                             await doufu.addWhiteName(message[2])
@@ -33,7 +58,8 @@ app.use(async (ctx, next) => {
                         await doufu.deleteWhiteName(message[2])
                         ctx.app.send(`GroupMessage ${content[2]} ${encode(`玩家${message[2]} 删除成功`)}`)
                     }
-                }
+                }*/
+				
 
                 if(message[0] === '#exec') {
                   let cmd = message.concat()
@@ -43,7 +69,7 @@ app.use(async (ctx, next) => {
 					ctx.app.send(`GroupMessage ${content[2]} ${encode(`指令${cmd.join(" ")}执行成功`)}`)
                     
                 }
-                if(message[0] === '#send') {
+                if(message[0] === '#send' || message[0] === '#tell' || message[0] === '#say') {
                     let cmd = message.concat()
                     cmd.shift()
                     console.log(cmd.join(" "))
@@ -52,6 +78,7 @@ app.use(async (ctx, next) => {
                     
                 }
             }catch (e) {
+				console.error(e.message);
                 await ctx.app.send(`PrivateMessage 296409654 ${encode(e.message)}`)
             }
         }
